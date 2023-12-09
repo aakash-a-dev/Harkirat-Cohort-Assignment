d75eaffc9ff69141dfda323e2d53367fddefdc88
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2023-12-09 22:45:49 +0530

    Count Vowel Question Done

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,25 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  const lowerCaseStr = str.toLowerCase();
+
+  // Define an array of vowels
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  // Initialize a variable to store the count of vowels
+  let vowelCount = 0;
+
+  // Iterate through each character in the string
+  for (let i = 0; i < lowerCaseStr.length; i++) {
+      // Check if the current character is a vowel
+      if (vowels.includes(lowerCaseStr[i])) {
+          // If it is, increment the vowel count
+          vowelCount++;
+      }
+  }
+
+  // Return the final count of vowels
+  return vowelCount;
 }
 
 module.exports = countVowels;
