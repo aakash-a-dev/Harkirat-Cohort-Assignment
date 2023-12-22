6198094c591fa2a8c52f93db7773ddede545c1a8
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2023-12-22 14:15:12 +0530

    Completed reate Limiter

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,16 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers['user-id'];
+  numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] || 0;
+  if( numberOfRequestsForUser[userId] > 5){
+    return res.sendStatus(404);
+  }
+  numberOfRequestsForUser[userId]++;
+  next();
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
