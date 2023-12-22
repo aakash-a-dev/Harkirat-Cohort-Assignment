d6dbed41beb436bc714c94ba7656cdcba2a27b0a
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2023-12-22 14:40:57 +0530

    Error Count question done

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -10,6 +10,8 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
+
+
 app.get('/user', function(req, res) {
   throw new Error("User not found");
   res.status(200).json({ name: 'john' });
@@ -22,5 +24,10 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+
+app.use(function(err, req, res, next){
+  res.status(404).json({"msg": "Exception endpoint returns a 404"});
+  errorCount++
+})
 
 module.exports = app;
