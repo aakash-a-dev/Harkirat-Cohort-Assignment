99ef55c5410b01befa04c66b00c46636f1c5ff11
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-02 22:18:36 +0530

    Updated code for Validation

--- week-4/react-basics/server/app.js
+++ week-4/react-basics/server/app.js
@@ -1,16 +1,35 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", (req, res) = {
-    
+app.post("/todo", (req, res) => {
+
+    const createPayload = req.body;
+    const paresdPayload = createTodo.safeParse(createPayload);
+
+    if (!paresedPayload.success) {
+        res.status(411).json({
+            msg: "You entered wrong input"
+        })
+        return
+    }
 })
 
 app.get("/todos", (req, res) => {
 
 })
 
-app.post("/completed", (req, res) => {
+app.put("/completed", (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+
     
+    if (!paresedPayload.success) {
+        res.status(411).json({
+            msg: "You entered wrong input"
+        })
+        return
+    }
 })
