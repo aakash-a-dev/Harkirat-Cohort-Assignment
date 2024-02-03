6572a95aafd13e8a58f2b149bfb09f52aad47862
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-03 12:04:20 +0530

    Api's Set up done

--- week-4/react-basics/server/app.js
+++ week-4/react-basics/server/app.js
@@ -1,27 +1,47 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+
+app.get("/", (req, res) => {
+    res.json({
+        msg:"Initialised"
+    })
+})
+app.post("/todo", async(req, res) => {
 
     const createPayload = req.body;
-    const paresdPayload = createTodo.safeParse(createPayload);
+    const parsedPayload = createTodo.safeParse(createPayload);
 
-    if (!paresedPayload.success) {
+    if (!parsedPayload.success) {
         res.status(411).json({
             msg: "You entered wrong input"
         })
         return
     }
-})
 
-app.get("/todos", (req, res) => {
+   await todo.create({
+        title: createPayload.title,
+        description: createPayload.description
+   })
     
+    res.json({
+        msg: "Todo Created"
+    })
 })
 
-app.put("/completed", (req, res) => {
+app.get("/todos", async(req, res) => {
+    const todos = await todo.find({});
+
+    res.json({
+        todos
+    })
+})
+
+app.put("/completed", async(req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
 
@@ -32,4 +52,16 @@ app.put("/completed", (req, res) => {
         })
         return
     }
+    await todo.update({
+        __id: req.body.id
+    }, {
+        completed: true
+    })
+    res.json({
+        msg: "Todo Marked as Completed"
+    })
 })
+
+app.listen(3000, () => {
+    console.log("Started port on 3000");
+});
